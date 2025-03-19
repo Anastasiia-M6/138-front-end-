91947fdd2e5d2164b51f14504303ea6085f82201
Anastasiia Mikhailova <mihailovaanastasia6@gmail.com>
2025-03-19 23:04:52 +0200

    сщььше

---
+++ java-script/01_lesson-first/js/zad.js
@@ -0,0 +1,20 @@
+//задание 1
+// let userName = 'userName';
+// console.log('hello' + "" + 'userName');
+// let lastName = 'lastName';
+// console.log('hello' + "" + 'userName' + "" + 'lastName' + "" + '!');
+
+//zad2
+//сумму в грн
+//результат :имя вы получите в долларах
+
+//то что думала что ок
+// let userName = 'userName';
+// let p1 = +prompt('100' + '$');
+// let p2 = +prompt('4000' + 'грн');
+// console.log(p1 + "*" + p2 + "="(p1 * p2));
+
+let p3 = prompt('имя');
+let p4 = +prompt('сумма в $');
+let res2 = p3 + " Ваша сума: " + p4 * 40 + 'грн';
+console.log(p3 + " Ваша сума: " + p4 * 40 + 'грн');
