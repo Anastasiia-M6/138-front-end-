4bb1779241a79d10db00eac8832f37cfba6d5a7e
Anastasiia Mikhailova <mihailovaanastasia6@gmail.com>
2025-05-21 20:22:43 +0300

    comm

---
+++ react_v138/lesson-one/src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div className="wrapper">
+    <section className="hero">
+      <div className="container hero_container">
+        <h1 className="title hero_title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi molestiae tempore odio sint, nihil unde voluptas iste animi ab nobis at sit, ipsam incidunt esse laudantium, exercitationem nemo laboriosam. Dicta?</h1>
+        <p className="hero_text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati debitis iste, magni saepe mollitia temporibus voluptas culpa molestiae unde voluptate cum aut accusamus amet nobis quisquam ratione qui ea dolorum?</p></div>
+      <button className="button">start</button>
+    </section>
+  </div>
+
+
+);
+
+
+
+// let root = document.getElementById('root');
+// let div = document.getElementById('div');
